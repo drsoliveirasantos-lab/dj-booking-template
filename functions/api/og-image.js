b112ae17894642c64fd9d2638f1f93d579c7b543
drsoliveirasantos-lab <drs.oliveirasantos@gmail.com>
2026-07-02 00:59:59 +0200

    Prepare OG image endpoint

--- functions/api/og-image.js
+++ functions/api/og-image.js
@@ -1,14 +1,3 @@
-const IMAGE_BASE64 = 'PLACEHOLDER';
-
 export async function onRequest() {
-  const binary = atob(IMAGE_BASE64);
-  const bytes = new Uint8Array(binary.length);
-  for (let i = 0; i < binary.length; i++) bytes[i] = binary.charCodeAt(i);
-  return new Response(bytes, {
-    headers: {
-      'content-type': 'image/jpeg',
-      'cache-control': 'public, max-age=31536000, immutable',
-      'access-control-allow-origin': '*'
-    }
-  });
+  return new Response('ok', { headers: { 'content-type': 'text/plain' } });
 }
